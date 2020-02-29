e5090ae00f6d3bf54689490895861d61a6645308
Vitor Mariano <vitorcor8@gmail.com>
2020-02-29 17:43:20 -0300

    Organização dos arquivos

---
+++ sucesso.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>O cadastro foi realizado com sucesso!</h1>
+    <meta http-equiv="refresh" content="10; URL='index.html'"/>
+</body>
+</html>
