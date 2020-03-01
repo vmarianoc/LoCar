95e091fcaecce9df8ac258a06c392f1b2346a753
Vitor Mariano <vitorcor8@gmail.com>
2020-03-01 02:37:31 -0300

    .

--- enviar.php
+++ enviar.php
@@ -1,42 +1,26 @@
 <?php
-    $erro = false;
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    $conn = new mysqli($server, $username, $password, $db);
+ 
 
     // Verifica se o POST tem algum valor
     if ( !isset( $_POST ) || empty( $_POST ) ) {
         $erro = 'Nada foi postado.';
     }
-    if ( ( ! isset( $cpf ) || ! is_numeric( $cpf ) ) && !$erro ) {
-        $erro = 'o CPF/ Passaporte deve ser um valor numérico.';
-    }
-    if ( ( ! isset( $telefone ) || ! is_numeric( $telefone ) ) && !$erro ) {
-        $erro = 'O Telefone ou Celular deve ser um valor numérico.';
-    }
-    if ( ( ! isset( $cnh ) || ! is_numeric( $cnh ) ) && !$erro ) {
-        $erro = 'O Número de Registro da CNH deve ser um valor numérico.';
-    }
-    if ( ( ! isset( $cep ) || ! is_numeric( $cep ) ) && !$erro ) {
-        $erro = 'O CEP deve ser um valor numérico.';
-    }
-    if ( ( ! isset( $numero ) || ! is_numeric( $numero ) ) && !$erro ) {
-        $erro = 'O Número deve ser um valor numérico.';
-    }
-    if ( ( ! isset( $email ) || ! filter_var( $email, FILTER_VALIDATE_EMAIL ) ) && !$erro ) {
-        $erro = 'Envie um email válido.';
-    }
-    if ( $erro ) {
-        echo $erro;
+    if(!$conect=mysqli_connect('localhost','root','','usuario')) die ('erro ao conectar');
+        $cpf = mysqli_real_escape_string($_POST['cpf']);
+        $sql = $conect->query("SELECT * FROM usuario WHERE cpf='$cpf'");
+    if(mysqli_num_rows($sql) > 0){
+        echo "Este CPF/ Passaporte já está cadastrado";
+        exit();
     } else {
 
-    
-        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-        $server = $url["host"];
-        $username = $url["user"];
-        $password = $url["pass"];
-        $db = substr($url["path"], 1);
-
-        $conn = new mysqli($server, $username, $password, $db);
-    
         $cpfBD = $_POST['cpf'];
         $nomeBD = $_POST['nome'];
         $emailBD = $_POST['email'];
@@ -58,7 +42,8 @@
             echo "Error: " . $sql . "" . mysqli_error($conn);
         }
         mysqli_close($conn);
+    }
 
-    } 
+ 
 
 ?>
